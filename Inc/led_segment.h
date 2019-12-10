e027041368df01a9f78962e5eafd2d8b143ca2de
Ruslan Terentiev <terentyev_ruslan@mail.ru>
2019-12-10 10:26:53 +0300

    Add files via upload

---
+++ Inc/led_segment.h
@@ -0,0 +1,14 @@
+/*
+ * led_segment.h
+ *
+ *  Created on: 7 но€б. 2016 г.
+ *      Author: Spectran
+ */
+
+#ifndef LED_SEGMENT_H_
+#define LED_SEGMENT_H_
+
+void Display_Char(uint8_t chr);
+void Display_Clear();
+
+#endif /* LED_SEGMENT_H_ */
